cb3ebf65522d6554f6f93a8e6d1d36d8dcf6b3dd
Ксения Нагишева <knagisheva@gmail.com>
2026-03-15 18:04:16 +0300

    Player UI fix

--- Architecture-RPG/Assets/Scripts/Player/PlayerController.cs
+++ Architecture-RPG/Assets/Scripts/Player/PlayerController.cs
@@ -106,4 +106,5 @@ public class PlayerController : MonoBehaviour
 
         aimTarget.position = ray.GetPoint(15f);
     }
+    
 }
--- Architecture-RPG/Assets/Scripts/Player/PlayerUIController.cs
+++ Architecture-RPG/Assets/Scripts/Player/PlayerUIController.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class PlayerUIController : MonoBehaviour
 {
     [SerializeField] private GameObject pausePanel;
     [SerializeField] private GameObject deathPanel;
     [SerializeField] private Image healthBar;
+    [SerializeField] private Image shootTimer;
 
     public void Pause(bool pauseOn)
     {
@@ -20,6 +22,21 @@ public class PlayerUIController : MonoBehaviour
 
     public void ReduceHealth(int currHealth)
     {
-        healthBar.fillAmount = currHealth / 100;
+        healthBar.fillAmount = currHealth * 0.01f;
+    }
+
+    public void MagicTimerUI(float coolDown)
+    {
+        shootTimer.fillAmount =0;
+        StartCoroutine(CoolMagic(coolDown));
+    }
+    
+    private IEnumerator CoolMagic(float coolDown)
+    {
+        for (int i=0; i<100; i++)
+        {
+            shootTimer.fillAmount += 0.01f;
+            yield return new WaitForSeconds(coolDown/100f);
+        }
     }
 }
