947c660dd9d661f0e22bd901b226aeb04a00e5f2
Ксения Нагишева <knagisheva@gmail.com>
2026-04-27 00:38:36 +0300

    Boss melee attacks variants

--- Architecture-RPG/Assets/Scripts/Mobs/MultipleBossWeaponsController.cs
+++ Architecture-RPG/Assets/Scripts/Mobs/MultipleBossWeaponsController.cs
@@ -1,15 +1,17 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MultipleWeaponsBossController : MonoBehaviour
 {
     [SerializeField] private GameObject[] weapons;
-    private int _currWeaponIndx;
-    
+    private int _currWeaponIndx=0;
+
     public void ChooseWeapon()
     {
         _currWeaponIndx = RandomBetween(0, weapons.Length);
-         weapons[_currWeaponIndx].SetActive(true);
-        
+        weapons[_currWeaponIndx].SetActive(true);
+        Debug.Log(_currWeaponIndx);
     }
 
     public void DeactivateWeapon()
